0afc718f62bc91844b91176d777052142c1f274d
Ama Asare <aasare@thoughtworks.com>
2019-11-14 12:24:54 -0800

    Update release notes content and format

--- bin/release.sh
+++ bin/release.sh
@@ -7,10 +7,9 @@ set -eu
 RELEASE_CHANGELOG=$(buildkite-agent meta-data get release-changelog)
 RELEASE_VERSION=$(buildkite-agent meta-data get release-version) 
 
-git tag -a "${RELEASE_VERSION}" -m "CHANGELOG: ${RELEASE_CHANGELOG} ~Created via buildkite build ${BUILDKITE_BUILD_URL}~"
+git tag -a "${RELEASE_VERSION}" -m "##CHANGELOG: ${RELEASE_CHANGELOG} ~Created via buildkite build ${BUILDKITE_BUILD_URL}~"
 git push origin "${RELEASE_VERSION}"
 echo "+++ :boom:" 
-echo "+++ created release ${RELEASE_VERSION}"
-echo "+++ for plugin ${BUILDKITE_REPO} "
-echo "+++ with CHANGELOG ${RELEASE_CHANGELOG}"
+echo "+++ created release ${BUILDKITE_REPO}#${RELEASE_VERSION}"
+echo "+++ with CHANGELOG: ${RELEASE_CHANGELOG}"
 echo "+++ ✅"
