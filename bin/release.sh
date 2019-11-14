5cc41235b2b16b0995e26c08803a76607f3d7ef1
Ama Asare <aasare@thoughtworks.com>
2019-11-14 12:20:04 -0800

    Specify buildkite build in release notes

--- bin/release.sh
+++ bin/release.sh
@@ -7,8 +7,10 @@ set -eu
 RELEASE_CHANGELOG=$(buildkite-agent meta-data get release-changelog)
 RELEASE_VERSION=$(buildkite-agent meta-data get release-version) 
 
-git tag -a "${RELEASE_VERSION}" -m "CHANGELOG. ${RELEASE_CHANGELOG}"
+git tag -a "${RELEASE_VERSION}" -m "CHANGELOG: ${RELEASE_CHANGELOG} ~Created via buildkite build ${BUILDKITE_BUILD_URL}~"
 git push origin "${RELEASE_VERSION}"
-echo "+++ :boom: created release ${RELEASE_VERSION} with CHANGELOG ${RELEASE_CHANGELOG}"
-
-echo "✅"
+echo "+++ :boom:" 
+echo "+++ created release ${RELEASE_VERSION}"
+echo "+++ for plugin ${BUILDKITE_REPO} "
+echo "+++ with CHANGELOG ${RELEASE_CHANGELOG}"
+echo "+++ ✅"
